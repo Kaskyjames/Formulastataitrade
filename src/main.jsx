3158fea63042c41e2ab2e37f89bb44443a23f17d
Kaskyjames <izuchukwujameskasky@gmail.com>
2025-06-06 12:22:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FormulaStatAI from "./FormulaStatAI";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FormulaStatAI />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FormulaStatAI from "./FormulaStatAI";
+import AboutUs from "./pages/AboutUs";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FormulaStatAI />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
