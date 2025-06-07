1d4c143e538ed9219cc69232f8966ee1a3034cc5
Kaskyjames <izuchukwujameskasky@gmail.com>
2025-06-07 14:49:15 +0200

    Update LogoHero.jsx

--- src/components/LogoHero.jsx
+++ src/components/LogoHero.jsx
@@ -5,7 +5,7 @@ const LogoHero = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-black">
       <img
-        src="/formulastatlogo.png"
+        public="/formulastatlogo.png"
         alt="Formula Stat Logo"
         className="w-48 h-48 transition-all duration-700 ease-in-out transform hover:scale-110 hover:rotate-12 animate-logo-entry"
       />
