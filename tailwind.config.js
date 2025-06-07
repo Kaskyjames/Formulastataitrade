73b0dd352961bb89b4a323dcace97ed68c159046
Kaskyjames <izuchukwujameskasky@gmail.com>
2025-06-07 16:53:15 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'logo-entry': 'zoomRotate 1s ease-in-out',
+      },
+      keyframes: {
+        zoomRotate: {
+          '0%': { transform: 'scale(0) rotate(0deg)' },
+          '100%': { transform: 'scale(1) rotate(360deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
-theme: {
-  extend: {
-  animation: {
-    'logo-entry': 'zoomRotate 1s ease-in-out',
-  },
-  keyframes: {
-    zoomRotate: {
-      '0%': { transform: 'scale(0) rotate(0deg)' },
-      '100%': { transform: 'scale(1) rotate(360deg)' },
-    },
-  },
-  }
