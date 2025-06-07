e0b365bea0f57bc18758d0d9a3dcb936d5b8eb59
Kaskyjames <izuchukwujameskasky@gmail.com>
2025-06-07 04:13:27 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
+import ServicesSection from "./pages/ServicesSection";
 
+<ServicesSection />
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // Central app file with routing
+import App from "./App"; 
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
