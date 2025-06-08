102135043a0bc5cfee196bca656261312b4e3cad
Kaskyjames <izuchukwujameskasky@gmail.com>
2025-06-08 02:23:53 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
 import FormulaStatAITrade from "./FormulaStatAI"; // Homepage
 import AboutUs from "./pages/AboutUs";            // About Page
-import Services from "./pages/Services";          // Services Page (new)
+import Services from "./pages/Services";          // Services Page
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Home Route */}
         <Route path="/" element={<FormulaStatAITrade />} />
-
-        {/* About Route */}
         <Route path="/about" element={<AboutUs />} />
-
-        {/* Services Route */}
         <Route path="/services" element={<Services />} />
       </Routes>
     </BrowserRouter>
